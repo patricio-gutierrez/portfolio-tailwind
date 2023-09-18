02a9df4cbe807702f71f29381e746afc44436828
Diego Patricio Gutierrez <me@patriciog.com>
2023-09-18 14:18:18 -0600

    Added config to tailwing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        darkerBlue: "hsl(212, 75%, 16%)",
+        lighterBlue: "hsl(211, 70%, 26%)",
+        seaBlue: "hsl(214, 65%, 35%)",
+        coldBlue: "hsl(208, 61%, 44%)",
+      },
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
+
+// https://colorhunt.co/palette/0a26471442722052952c74b3
